4452459ee4da5031bf8d85d31bf74ecb3fb94903
Carlos Duarte <e_craud@hotmail.com>
2025-09-29 21:58:39 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+    return (
+        <>
+            <Button variant="contained">Hello</Button>
+        </>
+    );
+}
+
+export default App;
